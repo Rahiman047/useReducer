bcbf6578cd9a464790e038f13574c0843e59edf5
Rahiman <abdurrahiman100@gmail.com>
2023-10-12 18:05:32 +0530

    setup

--- src/App.1.jsx
+++ src/App.1.jsx
@@ -1,41 +1,23 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 import ShowUserNames from "./ShowUserNames";
-import "./App.css";
+import { reducer, defaultState } from "./App";
 
-const userData = [
-  { name: "Rahiman", id: 1 },
-  { name: "Abdur", id: 2 },
-  { name: "Shaik", id: 3 },
-  { name: "John", id: 4 },
-  { name: "Jane", id: 5 },
-];
-
-const defaultState = {
-  people: userData,
-};
-
-const reducer = () => {};
-
-function App() {
+export function App() {
   const [state, dispatch] = useReducer(reducer, defaultState);
 
   // const [data, setData] = useState(userData);
-
-  const removeParticularUser = (id) => {
-    // const newData = data.filter((eachData) => {
-    //   return eachData.id !== id;
-    // });
-    // setData(newData);
-  };
-
-  const clearAllUsers = () => {
-    // setData([]);
-  };
-
-  const resetUsers = () => {
-    // setData(userData);
-  };
-
+  // const removeParticularUser = (id) => {
+  //   const newData = data.filter((eachData) => {
+  //     return eachData.id !== id;
+  //   });
+  //   setData(newData);
+  // };
+  // const clearAllUsers = () => {
+  //   setData([]);
+  // };
+  // const resetUsers = () => {
+  //   setData(userData);
+  // };
   return (
     <div>
       <h1>UserData</h1>
@@ -58,5 +40,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
